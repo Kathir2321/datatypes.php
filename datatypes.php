e4b177867697b3b94efec746f43231905a604f85
Kathir2321 <142398049+Kathir2321@users.noreply.github.com>
2023-08-17 18:46:15 +0530

    Add files via upload

---
+++ datatypes.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $x = "Hi prasath";
+    $y = "Hi kathir";
+    echo $x;
+    echo $y;
+    ?><br>
+    <?php
+    $x = 1234;
+    $y = 4321;
+    var_dump($x,$y);
+    ?><br>
+    <?php
+    $x = 1.234;
+    $y = 4.321;
+    var_dump($x,$y);
+    ?><br>
+    <?php
+    $x = true;
+    $y = false;
+    var_dump($x,$y);
+    ?><br>
+    <?php
+    $hotels = array("Sivaraj","Chenneys","GRT");
+    var_dump($hotels);
+    ?><br>
+    <?php
+    $x = "Siva";
+    $x = "NULL";
+    var_dump($x);
+    ?>
+</body>
+</html>
